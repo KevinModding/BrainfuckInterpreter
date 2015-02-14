954e42acafad8a40d28ef639797c58c2b0e81a4e
Kevin Modding <KevinModding@users.noreply.github.com>
2015-02-14 11:56:33 -0500

    Fixed issue #1
    There was a bug in the loops operators.

--- main.c
+++ main.c
@@ -8,10 +8,12 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#define DEBUG 0   //set to 1 to print "Hello World!" as a test
 
 typedef unsigned char byte;
 
 /*Operators*/
+
 #define INCREMENT_PTR '>'
 #define	DECREMENT_PTR '<'
 #define	INCREMENT_VAL '+'
@@ -27,7 +29,7 @@ typedef unsigned char byte;
 #define MAX_NUMBER_CELLS 30000 //Each cells are a single byte
 #define MAX_CODE_SIZE 100000 //Max size of the source code, that should be enough
 
-byte Cells[MAX_NUMBER_CELLS]; //We will have to zero them
+byte Cells[MAX_NUMBER_CELLS]; //We will have to null them
 
 int main(int argc, char** argv){
 
@@ -40,34 +42,41 @@ int main(int argc, char** argv){
 	memset(src, 0, MAX_CODE_SIZE);
 	memset(Cells, 0, MAX_NUMBER_CELLS);
 
-	if (argc < 2){
-		printf("Usage:Brainfuck.exe \"Input file\"\nExample: Brainfuck.exe \"C:\\test.txt\"\n\n");
-		getchar();
-		return -1;
-	}
-
-
-	/*Get Source code*/
-	FILE* in = fopen(argv[1], "r");
-	if (in != NULL){
-		fseek(in, 0, SEEK_END);
-		flen = ftell(in);
-		if (flen > MAX_CODE_SIZE){
-			fclose(in);
-			printf("File is too big! Max length: %i\n", MAX_CODE_SIZE);
+	if (!DEBUG){ //This variable is set manually at the beginning of the code
+		if (argc < 2){
+			printf("Usage:Brainfuck.exe \"Input file\"\nExample: Brainfuck.exe \"C:\\test.txt\"\n\n");
 			getchar();
 			return -1;
 		}
-		fseek(in, 0, SEEK_SET);
-		fread(src, flen, 1, in);
-		fclose(in);
-		
+		/*Get Source code*/
+		FILE* in = fopen(argv[1], "r");
+		if (in != NULL){
+			fseek(in, 0, SEEK_END);
+			flen = ftell(in);
+			if (flen > MAX_CODE_SIZE){
+				fclose(in);
+				printf("File is too big! Max length: %i\n", MAX_CODE_SIZE);
+				getchar();
+				return -1;
+			}
+			fseek(in, 0, SEEK_SET);
+			fread(src, flen, 1, in);
+			fclose(in);
+
+		}
+		else{
+			printf("File cannot be read! Try to move it to your desktop?\n");
+			getchar();
+			return -1;
+		}
+
 	}
 	else{
-		printf("File cannot be read! Try to move it to your desktop?\n");
-		getchar();
-		return -1;
+		/*This debug string will print "Hello World!" */
+		strcpy(src, "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.");
 	}
+
+
 
 	int srcLen = strlen(src);
 
@@ -105,26 +114,35 @@ int main(int argc, char** argv){
 			break;
 
 
-			
+		/*Gotta test*/
 		case START_LOOP:
+			/*incase we start a loop with 0 as a value*/
 			if (Cells[curCell] == 0){
-				for (int a = i; a < srcLen; a++){
-					if (src[a] == END_LOOP){
-						i = a; break;
+				int occurences = 0;  //number of loops after it
+				for (int a = i+1; a > srcLen; a++){
+					if (src[a] == START_LOOP){
+						occurences++; //we found another loop, we will have to skip his 'START_LOOP'
+					}
+					else if (src[a] == END_LOOP){
+						if (occurences > 0){ occurences--; } //we have skipped a loop
+						else{ break; } //gotta take a break
 					}
 
-					if (a == srcLen -1){ return -1; }
 				}
-
 			}
 			break;
 
 
 		case END_LOOP:
 			if (Cells[curCell] != 0){
-				for (int a = i; a > 0; a--){
+				int occurences = 0;  //number of loops before it
+				for (int a = i-1; a > 0; a--){
 					if (src[a] == START_LOOP){
-						i = a - 1; break;
+						if (occurences > 0){ occurences--; }
+						else{ i = a - 1; break; }
+					}
+					else if (src[a] == END_LOOP){
+						occurences++;
 					}
 					if (a == 1){ return -1; }
 				}
